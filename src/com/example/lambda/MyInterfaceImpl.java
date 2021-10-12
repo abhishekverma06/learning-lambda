29577a81a992333b5ee30c028e996b73b9c09673
abhishekverma06 <abhishekverma06@yahoo.com>
2021-10-12 19:51:11 +0530

    Trying Lambda expressiona

---
+++ src/com/example/lambda/MyInterfaceImpl.java
@@ -0,0 +1,9 @@
+package com.example.lambda;
+
+public class MyInterfaceImpl implements MyInterface
+{
+    @Override
+    public void sayHello() {
+        System.out.println("I am saying Hello");
+    }
+}
